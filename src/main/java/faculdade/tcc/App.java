3a84dc99da65c6802bc872a3fffd9ae96e51d442
unknown <gustavo.bianchi.gonzaga@gmail.com>
2016-01-09 11:34:11 -0200

    meu primeiro commit

---
+++ src/main/java/faculdade/tcc/App.java
@@ -0,0 +1,13 @@
+package faculdade.tcc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
